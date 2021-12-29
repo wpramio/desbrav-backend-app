b0826925a04300582c68c2090dea88af82e21952
wpramio <willianpramio@hotmail.com>
2021-12-29 20:25:05 -0300

    feat: setup scaffold for User
    Generated with
    `rails g scaffold User name:string email:string password_digest:string`

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace 'api', defaults: { format: 'json' } do
     namespace 'v1' do
+      resources :users
       resources :product_categories do
         get 'products', to: 'product_categories#products'
       end
